7bc3bf89cf89e397176b3fd6b0364f71c836a88c
mab <madeline.blount@gmail.com>
2022-12-06 20:48:44 -0500

    add airtable, netlify function

--- netlify/functions/assign-advocate/assign-advocate.js
+++ netlify/functions/assign-advocate/assign-advocate.js
@@ -1,14 +1,60 @@
+const Airtable = require('airtable');
+
 exports.handler = function(event, context, callback) {
   const data = JSON.parse(event.body);
   const { user } = data;
+
+  var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appiZpVxsiS1Ev5Zv');
+  // currently TEST: STAGING BASE 
+
+  let emails = []
+  let loginResponse;
 
   const responseBody = {
     app_metadata: {
       roles: ["advocate"]
     }
   };
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(responseBody)
+
+  console.log(user);
+
+  base('Partner organizations').select({
+    fields: ["Report Form Logins"]
+  }).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+    records.forEach(function(record) {
+      emails.push(record.get('Report Form Logins'));
+      console.log(record.get('Report Form Logins'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+  }, function done(err) {
+    if (err) { console.error(err); return; }
+    checkEmails();
   });
+
+  function checkEmails() {
+    approved = False;
+    for (i = 0; i < emails.length; i++) {
+      if (emails[i] == user.user_metadata.email) {
+        approved = True;
+        loginResponse = {
+          statusCode: 200,
+          body: JSON.stringify(responseBody)
+        }
+        break;
+      }
+    }
+    if (approved == False) {
+      loginResponse = {
+        statusCode: 500,
+        body: null
+      }
+    }
+  }
+
+  callback(null, loginResponse);
 };
