314d840f17627e7c6397a953c8e7bd390d349b86
mab <madeline.blount@gmail.com>
2022-12-09 15:12:44 -0500

    submit-advocate work

--- netlify/functions/submit-advocate/submit-advocate.js
+++ netlify/functions/submit-advocate/submit-advocate.js
@@ -1,15 +1,15 @@
 const Airtable = require('airtable');
 
-exports.handler = function(event, context, callback) {
+exports.handler = async function(event, context, callback) {
   var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appiZpVxsiS1Ev5Zv');
   // TEST for now, staging base
 
   console.log(event.body);
-  console.log(event.body.client)
+  const fields = await parseMultipartForm(event)
 
   base('All reports').create({
     "Report Type": "CCHR reports",
-    "@name": ""
+    "@name": fields.client.split(",")
   }, function(err, record) {
     if (err) {
       console.error(err);
@@ -21,5 +21,47 @@ exports.handler = function(event, context, callback) {
       body: JSON.stringify({ message: 'report submitted' })
     })
   });
+
+  // busboy for parsing form
+  function parseMultipartForm(event) {
+    return new Promise((resolve) => {
+      const fields = {};
+      // let's instantiate our busboy instance!
+      const busboy = new Busboy({
+        // it uses request headers
+        headers: event.headers
+      });
+      // before parsing anything, we need to set up some handlers.
+      // whenever busboy comes across a file ...
+      busboy.on(
+        "file",
+        (fieldname, filestream, filename, transferEncoding, mimeType) => {
+          // ... we take a look at the file's data ...
+          filestream.on("data", (data) => {
+            // ... and write the file's name, type and content into `fields`.
+            fields[fieldname] = {
+              filename,
+              type: mimeType,
+              content: data,
+            };
+          });
+        }
+      );
+
+      // whenever busboy comes across a normal field ...
+      busboy.on("field", (fieldName, value) => {
+        // ... we write its value into `fields`.
+        fields[fieldName] = value;
+      });
+
+      // once busboy is finished, we resolve the promise with the resulted fields.
+      busboy.on("finish", () => {
+        resolve(fields)
+      });
+
+      // now that all handlers are set up, we can finally start processing our request!
+      busboy.write(event.body);
+    });
+  }
 }
 
