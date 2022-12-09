2e8b0cf4ee1964e1af03960913f793062e460737
mab <madeline.blount@gmail.com>
2022-12-08 19:20:54 -0500

    read-advocate list

--- netlify/functions/read-advocate/read-advocate.js
+++ netlify/functions/read-advocate/read-advocate.js
@@ -6,10 +6,6 @@ exports.handler = function(event, context, callback) {
   const token = data.access_token;
   let decoded = jwt_decode(token);
 
-  let responseBody = {
-    clientList: ""
-  }
-
   if (decoded.app_metadata.roles[0] == 'advocate') {
     var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appiZpVxsiS1Ev5Zv');
     // currently TEST: STAGING BASE 
@@ -23,8 +19,18 @@ exports.handler = function(event, context, callback) {
     }).eachPage(function page(records, fetchNextPage) {
       // This function (`page`) will get called for each page of records.
       records.forEach(function(record) {
-        responseBody.clientList = record.get("Client List").join(",");
-        console.log(responseBody);
+        let clients = record.get("Client List");
+        let clientList = [];
+        for (i = 0; i < clients.length; i++) {
+          // get user info for every item in client list
+          base('User information').find(clients[i], function(err, record) {
+            if (err) { console.error(err); return; }
+            clientList.push({
+              clientName: record.get("Name")
+            });
+          });
+        }
+        console.log(clientList)
       });
 
       // If there are no more records, `done` will get called.
@@ -33,7 +39,7 @@ exports.handler = function(event, context, callback) {
       if (err) { console.error(err); return; }
       callback(null, {
         statusCode: 200,
-        body: JSON.stringify(responseBody)
+        body: JSON.stringify(clientList)
       });
     });
 
