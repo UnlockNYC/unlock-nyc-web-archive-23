b3d4ee092aa7dfb21ca82ab1f01f40255ab39833
MAB <madeline.blount@gmail.com>
2023-02-15 17:34:59 -0500

    change assign-advocate to prod

--- netlify/functions/assign-advocate/assign-advocate.js
+++ netlify/functions/assign-advocate/assign-advocate.js
@@ -4,8 +4,8 @@ exports.handler = function(event, context, callback) {
   const data = JSON.parse(event.body);
   const { user } = data;
 
-  var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appiZpVxsiS1Ev5Zv');
-  // currently TEST: STAGING BASE 
+  var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('app3RonGnLm3P4aVF');
+  // currently PROD REPORTS BASE
 
   let approval = false;
 
@@ -21,19 +21,23 @@ exports.handler = function(event, context, callback) {
     body: ""
   };
 
+  // field ids, airtable 
+  let emailField = "fldDdJrgCAe1YD1xv";
+  let advNameField = "fldI6KDv6htRgpemE";
+  let orgNameField = "fld04CrtAkQn3CYmR"; // the column labeled 'for online form', just string
+
   // query airtable, 
   // check for e-mail in approved partner list
   base('Advocates').select({
-    fields: ["fld507vee2P1MzsmW", "fldsQvRzwPtcUS1Ve", "fldeT7EYWva8lEMJh"], // email, name, org name
+    fields: [emailField, advNameField, orgNameField], // PROD BASE - email, name, org name
     returnFieldsByFieldId: true
   }).eachPage(function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
     records.forEach(function(record) {
-      let email = record.get("fld507vee2P1MzsmW"); // email address column, advocate table
+      let email = record.get(emailField); // PROD BASE - email address column, advocate table
       if (email.indexOf(user.email) > -1) {
         approval = true;
         console.log("MATCH, REQUEST APPROVED");
-        responseBody.app_metadata.org = record.get("fldeT7EYWva8lEMJh"); // org name
+        responseBody.app_metadata.org = record.get(orgNameField); // PROD BASE - org name
       }
     });
     // If there are no more records, `done` will get called.
