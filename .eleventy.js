27340a83313c9351287662e6403caec61082af85
mab <madeline.blount@gmail.com>
2023-06-02 19:03:36 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 // configuration file
 
-// add styles/css, scripts, etc. folders!
 module.exports = function (eleventyConfig) {
+  // pass through: treat specific files/directories as static, not templates or HTML
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("scripts");
+  eleventyConfig.addPassthroughCopy("start/secure/advocate/report/schema.json");
+  
   eleventyConfig.setLiquidOptions({
     dynamicPartials: true,
     strictFilters: false, // renamed from `strict_filters` in Eleventy 1.0
